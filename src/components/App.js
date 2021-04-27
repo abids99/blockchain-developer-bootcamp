cdc73ae16f3c8edecfc2330720f03e0a0bca6ce1
Shaik Mohammad Abid <31583251+abids9999@users.noreply.github.com>
2021-04-27 16:43:46 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import Web3 from 'web3'
-import Token from '../abis/Token.json'
+import { connect } from 'react-redux'
+import {
+ loadWeb3,
+ loadAccount,
+ loadToken,
+ loadExchange
+} from '../store/interactions'
+import { accountSelector } from '../store/selectors'
 
 class App extends Component {
   componentWillMount() {
-    this.loadBlockchainData()
+    this.loadBlockchainData(this.props.dispatch)
   }
 
-  async loadBlockchainData() {
-    const web3 = new Web3(window.ethereum)
+  async loadBlockchainData(dispatch) {
+    const web3 = loadWeb3(dispatch)
+    const network = await web3.eth.net.getNetworkType()
     const networkId = await web3.eth.net.getId()
-    const accounts = await web3.eth.getAccounts()
-    const token = new web3.eth.Contract(Token.abi, Token.networks[networkId].address)
-    const totalSupply = await token.methods.totalSupply().call()
-    console.log("totalSupply", totalSupply)
+    const accounts = await loadAccount(web3, dispatch)
+    const token = loadToken(web3, networkId, dispatch)
+    const exchange = loadExchange(web3, networkId, dispatch)
   }
 
   render() {
+    console.log(this.props.account)
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -108,4 +116,10 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    account: accountSelector(state)
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import 'bootstrap/dist/css/bootstrap.css';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import configureStore from './store/configureStore'
+import 'bootstrap/dist/css/bootstrap.css'
+import * as serviceWorker from './serviceWorker'
 
+ReactDOM.render(
+	<Provider store={configureStore()}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+)
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
+/// If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 //reportWebVitals();
-serviceWorker.unregister();
+serviceWorker.unregister()
