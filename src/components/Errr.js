f008e7ab0bbb395975224091f557486b89ead98e
abids99 <abidsp9999@gmail.com>
2021-08-17 08:08:04 +0530

    My-17.3

--- src/components/Errr.js
+++ src/components/Errr.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { OverlayTrigger, Tooltip, } from 'react-bootstrap'
 
 class Errr extends Component {
-  componentDidMount() {
-    this.loadBlockchainData(this.props)
-  }
-
-  async loadBlockchainData(props) {
-    
-  }
-
   render() {
     return (
-      <div className="content bg-light text-black">
-      
-        Contract not loaded please change to Ropsten Test Network
+      <div className="content-cus">  
+        <div className="horizontal-split">	
+          
+          <div className="full bg-light text-dark">
+        		<div>
+                  
+            </div>
+            
+          </div>
+
+          <div className="full bg-light text-dark">
+            <div>
+        			   
+          	</div>
+          	
+        	</div>      
+       
+        </div>
+
+       <div className="horizontal-split"> 
+          
+          <div className="full  bg-light text-dark">
+            <div className="center-cus-text">
+              If Metamask not installed please install metamask      
+            </div>
+            <div className="center-cus-l"> 
+              <a href="https://metamask.io/"><input type="button" bg="dark" text="info" value='Click Here'></input></a>
+            </div>
+          </div>
+
+          <div className="full bg-light text-dark">
+            <div className="center-cus-text">
+              if Exchange is not loaded please change to Ropsten Test Network      
+            </div>
+            <div className="center-cus">  
+              <a href="https://docs.cargo.build/guides/changing-your-network-with-metamask"><input type="button" bg="dark" text="info" value='Click Here'></input></a>
+            </div> 
+          </div>      
+       
+        </div>   
+
       </div>
-      
+
     )
   }
 }
