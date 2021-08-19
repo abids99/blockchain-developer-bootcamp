abd3d2a03ec1fddbb17c48bf8a51ad9da2f28e06
abids99 <abidsp9999@gmail.com>
2021-08-19 12:41:58 +0530

    Revert "My-17.6"
    This reverts commit 41c4eb173d863b3bffd950a9b62a7c9218b71e44.

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -9,10 +9,9 @@ contract Token {
     string public symbol = "SMAR";
     uint256 public decimals = 18;
     uint256 public totalSupply;
-    //Mappings
     mapping(address => uint256) public balanceOf;    
     mapping(address => mapping(address => uint256)) public allowance;
-    // Events
+    // Event
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
