8e3754824dc144dfcd3d684d1c21af50ad2ee861
abids99 <abidsp9999@gmail.com>
2021-08-25 16:40:52 +0530

    19.7.8

--- src/components/Select.js
+++ src/components/Select.js
@@ -18,7 +18,7 @@ class Select extends Component {
               
             </div>
             <div className="center-button"> 
-              <a href="/CV" class="button-styling">Cv</a>
+              <a href="#/CV" className="button-styling">Cv</a>
             </div>
           </div>
 
@@ -31,7 +31,7 @@ class Select extends Component {
             
             </div>
             <div className="center-button">  
-              <a href="/Exchange" class="button-styling">Exchange</a>
+              <a href="#/Exchange" className="button-styling">Exchange</a>
             </div> 
           </div>      
        
