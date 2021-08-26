0aff34b630f4c020a12b76b982510e088ad78b4c
abids99 <abidsp9999@gmail.com>
2021-08-26 09:41:04 +0530

    19.7.9

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,13 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a className="navbar-brand" href="#/">SMAR Token Exchange</a>
+        <a className="navbar-brand" href="#">SMAR Token Exchange</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
+        <div className="center-button-nav">
+          <a href="#/" className="navbar-brand">Home</a>
+        </div>
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
             <a
