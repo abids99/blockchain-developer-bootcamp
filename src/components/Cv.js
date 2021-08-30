f97ee352e3c1bd942549e6fdcaadbdff33a516a2
abids99 <abidsp9999@gmail.com>
2021-08-30 16:25:13 +0530

    19.10.0

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-//import ReactDOM from 'react-dom'
-//import { OverlayTrigger, Tooltip, } from 'react-bootstrap'
+import CvNav from './CvNav'
 
 class Cv extends Component {
   render() {
     return (
-      <div className="content-cus">  
-        <div className="horizontal-split"> 
-          <div className="full  bg-light text-dark">
-            <div className="center-cus-text">
-              CV is not yet Deployed
-            </div>
-          </div>      
-        </div>   
+      <div>
+        <CvNav />,
+        <div className="content-cus">  
+          <div className="horizontal-split"> 
+            <div className="full  bg-light text-dark">
+              <div className="center-cus-text">
+                CV is not yet Deployed
+              </div>
+            </div>      
+          </div>   
+        </div>
       </div>
     )
   }
