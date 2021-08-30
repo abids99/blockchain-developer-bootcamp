072d23e0fd36cb80a60158826769d4dac8932004
abids99 <abidsp9999@gmail.com>
2021-08-30 17:18:30 +0530

    19.14

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PortfolioNav from './PortfolioNav'
+import PortfolioContent from './PortfolioContent'
 
 class Portfolio extends Component {
   render() {
     return (
       <div>
         <PortfolioNav />,
-        <div className="content-cus">  
-          <div className="horizontal-split"> 
-            <div className="full  bg-light text-dark">
-              <div className="center-cus-text">
-                Portfolio is not yet Deployed
-              </div>
-            </div>      
-          </div>   
-        </div>
+        <PortfolioContent />
       </div>
     )
   }
