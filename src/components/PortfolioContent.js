3633384aa60e371cdd83283df7dc0de66cf90923
abids99 <abidsp9999@gmail.com>
2021-08-30 17:19:07 +0530

    19.14.5

---
+++ src/components/PortfolioContent.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PortfolioNav from './PortfolioNav'
+
+class PortfolioContent extends Component {
+  render() {
+    return (
+      <div>
+        <PortfolioNav />,
+        <div className="content-cus">  
+          <div className="horizontal-split"> 
+            <div className="full  bg-light text-dark">
+              <div className="center-cus-text">
+                Portfolio is not yet Deployed
+              </div>
+            </div>      
+          </div>   
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps)(PortfolioContent)
