19e9826d1ef7e2fd14ff6d794903bcbec5732aea
abids99 <abidsp9999@gmail.com>
2021-09-18 16:12:05 +0530

    simple change

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,9 @@ const Token = artifacts.require("Token");
 const Exchange = artifacts.require("Exchange");
 
 module.exports = async function (deployer) {
-  const accounts = await web3.eth.getAccounts()
-
   await deployer.deploy(Token);
+  
+  const accounts = await web3.eth.getAccounts()
 
   const feeAccount = accounts[0]
   const feePercent = 10
